7e8df417776b38262b0126e537ab486551e186d6
PoPowerYang <yangsunsunshine@gmail.com>
2019-07-17 14:50:36 -0400

    favoriate button animation added, provider added, notifiers and listeners installed

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
@@ -9,10 +12,16 @@ class ProductDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final productId = ModalRoute.of(context).settings.arguments as String;
+    final productId = 
+      ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct 
+      = Provider.of<ProductsProvider>(
+        context, 
+        listen: false, //will not rebuild when data changes
+      ).findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
